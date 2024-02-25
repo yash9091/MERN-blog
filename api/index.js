9b3c1b749ff6b945b6a5160e827ed7e5423f5003
Yash Singh <yash26985@gmail.com>
2024-02-25 22:14:18 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -14,3 +14,7 @@ const app =express();
 app.listen(3000, ()=> {
     console.log('Server  is running on port 3000')
 });
+
+app.get('/test',(req,res)=> {
+    res.json({message:'API is working'});
+})
