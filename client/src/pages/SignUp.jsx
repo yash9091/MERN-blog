3ab06dfbda6e13015ffd0ed25b78836cb044126c
Yash Singh <yash26985@gmail.com>
2024-02-28 23:28:58 +0530

    complete signin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -101,7 +101,7 @@ export default function SignUp() {
             {/* <OAuth /> */}
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Have an account?</span>
+            <span> Have an account?</span>
             <Link to='/sign-in' className='text-blue-500'>
               Sign In
             </Link>
