328878438863db620214e2041f41a6ab555b451e
Yash Singh <yash26985@gmail.com>
2024-03-03 18:33:43 +0530

    complete all the componets of dashboard

---
+++ client/src/components/DashUsers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashUsers() {
+  return (
+    <div>
+      DashUsers
+    </div>
+  )
+}
---
+++ client/src/components/DashboardComp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashboardComp() {
+  return (
+    <div>
+      DashboardComp
+    </div>
+  )
+}
