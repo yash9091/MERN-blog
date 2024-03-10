46be0e9642091da9dace10d17a6a6c92fe1d70b3
Yash Singh <yash26985@gmail.com>
2024-03-10 14:52:55 +0530

    add update post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute copy'
 import CreatePost from './pages/CreatePost'
+import UpdatePost from './pages/UpdatePost'
 
 export default function App() {
   return (
@@ -30,7 +31,8 @@ export default function App() {
 
       <Route element={<OnlyAdminPrivateRoute/>}>
        <Route path='/create-post' element={<CreatePost/>}></Route>
-       
+       <Route path='/update-post/:postId' element={<UpdatePost/>}></Route>
+
       </Route>
 
        
