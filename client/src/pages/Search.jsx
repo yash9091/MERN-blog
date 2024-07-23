95e94191b0ee37f77feb7a1b41a9b193a295771f
Yash Singh <yash26985@gmail.com>
2024-07-23 17:06:16 +0530

    update deployed backend url

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -36,7 +36,7 @@ export default function Search() {
     const fetchPosts = async () => {
       setLoading(true);
       const searchQuery = urlParams.toString();
-      const res = await fetch(`/api/post/getposts?${searchQuery}`);
+      const res = await fetch(`https://mern-blog-api-beta.vercel.app/api/post/getposts?${searchQuery}`);
       if (!res.ok) {
         setLoading(false);
         return;
--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -31,7 +31,7 @@ const {postId} = useParams();
 useEffect(() => {
     try {
       const fetchPost = async () => {
-        const res = await fetch(`/api/post/getposts?postId=${postId}`);
+        const res = await fetch(`https://mern-blog-api-beta.vercel.app/api/post/getposts?postId=${postId}`);
         const data = await res.json();
         if (!res.ok) {
           console.log(data.message);
